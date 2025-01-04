da9f732c860cf10ce12f42dc50a1dd9f7a273a94
Global Ads Media <116465160+gamgsp@users.noreply.github.com>
2025-01-04 21:18:06 +0530

    packages update
    packages update

--- depends/packages/libICE.mk
+++ depends/packages/libICE.mk
@@ -1,12 +1,12 @@
-package=libXau
-$(package)_version=1.0.8
+package=libICE
+$(package)_version=1.0.9
 $(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=fdd477320aeb5cdd67272838722d6b7d544887dfe7de46e1e7cc0c27c2bea4f2
-$(package)_dependencies=xproto
+$(package)_sha256_hash=8f7032f2c1c64352b5423f6b48a8ebdc339cc63064af34d66a6c9aa79759e202
+$(package)_dependencies=xtrans xproto
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared
+  $(package)_config_opts=--disable-static --disable-docs --disable-specs --without-xsltproc
   $(package)_config_opts_linux=--with-pic
 endef
 
--- depends/packages/libSM.mk
+++ depends/packages/libSM.mk
@@ -1,12 +1,12 @@
-package=libXau
-$(package)_version=1.0.8
+package=libSM
+$(package)_version=1.2.2
 $(package)_download_path=http://xorg.freedesktop.org/releases/individual/lib/
 $(package)_file_name=$(package)-$($(package)_version).tar.bz2
-$(package)_sha256_hash=fdd477320aeb5cdd67272838722d6b7d544887dfe7de46e1e7cc0c27c2bea4f2
-$(package)_dependencies=xproto
+$(package)_sha256_hash=0baca8c9f5d934450a70896c4ad38d06475521255ca63b717a6510fdb6e287bd
+$(package)_dependencies=xtrans xproto libICE
 
 define $(package)_set_vars
-  $(package)_config_opts=--disable-shared
+  $(package)_config_opts=--without-libuuid  --without-xsltproc  --disable-docs --disable-static
   $(package)_config_opts_linux=--with-pic
 endef
 
